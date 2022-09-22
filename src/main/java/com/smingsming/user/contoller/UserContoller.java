a4e28cdb51f40b220803f1c79b3ad4b6ed4d6adb
jhy <yuns_97@naver.com>
2022-09-22 16:03:33 +0900

    add user health check in controller

---
+++ src/main/java/com/smingsming/user/contoller/UserContoller.java
@@ -0,0 +1,21 @@
+package com.smingsming.user.contoller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user-server")
+public class UserContoller {
+
+    @Autowired
+    Environment env;
+
+    @GetMapping("/health_check")
+    public String status() {
+        return String.format("It's Working in Catalog Service on PORT %s",
+                env.getProperty("local.server.port"));
+    }
+}
